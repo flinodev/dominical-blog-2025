0db497fc2402e304723f30e0c65ac000103c5442
Francisco Lino <francisco@montebit.io>
2025-08-24 14:26:39 -0600

    chore: update description

--- src/site.config.ts
+++ src/site.config.ts
@@ -13,7 +13,7 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	// Used as the default description meta property and webmanifest description
-	description: "An opinionated starter theme for Astro",
+	description: "Espacio de trabajo para el la Estudio Bíblico de la Escuela Dominical",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
