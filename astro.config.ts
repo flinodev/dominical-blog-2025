fe2c510b4e0f75516f07aa2ed5fe968104d01338
Francisco Lino <francisco@montebit.io>
2025-08-26 22:17:36 -0600

    feat: add domain in config

--- astro.config.ts
+++ astro.config.ts
@@ -115,7 +115,7 @@ export default defineConfig({
   // https://docs.astro.build/en/guides/prefetch/
   prefetch: true,
   // ! Please remember to replace the following site property with your own domain
-  site: "http://astrocitrus.artemkutsan.pp.ua/",
+  site: "https://dominical-blog-2025.vercel.app/",
   vite: {
     build: {
       sourcemap: true, // Source maps generation
